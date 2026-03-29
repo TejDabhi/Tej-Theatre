3ec77f15ec192895f5ccb3974c4ee76f8ae32194
Tej <157304155+TejDabhi@users.noreply.github.com>
2026-03-29 17:18:12 +0530

    Updated dbConfig.js with database connection logic on 2026-03-29 11:47:58 UTC

---
+++ dbConfig.js
@@ -0,0 +1,1 @@
+const mongoose=require('mongoose'); mongoose.set('strictQuery',false); mongoose.connect(process.env.MONGODB_URI); const connection =mongoose.connection; connection.on('connected',()=>{ console.log('Mongo DB Connection is Successful'); }); connection.on('error',(err)=>{ console.log('Mongo DB Connection Failed'); console.error(err); });
